e091248d2139385b8006551b0e5d4be4c07accb9
nawal <nawal@bigvisionllc.com>
2022-07-27 12:22:48 +0000

    made tf2 compatible, still error (seg fault)

--- util/visualizer.py
+++ util/visualizer.py
@@ -21,7 +21,9 @@ class Visualizer():
             import tensorflow as tf
             self.tf = tf
             self.log_dir = os.path.join(opt.checkpoints_dir, opt.name, 'logs')
-            self.writer = tf.summary.FileWriter(self.log_dir)
+            with tf.compat.v1.Graph().as_default():
+                self.writer = tf.compat.v1.summary.FileWriter(self.log_dir)
+
 
         if self.use_html:
             self.web_dir = os.path.join(opt.checkpoints_dir, opt.name, 'web')
@@ -45,12 +47,12 @@ class Visualizer():
                     s = BytesIO()
                 scipy.misc.toimage(image_numpy).save(s, format="jpeg")
                 # Create an Image object
-                img_sum = self.tf.Summary.Image(encoded_image_string=s.getvalue(), height=image_numpy.shape[0], width=image_numpy.shape[1])
+                img_sum = self.tf.compat.v1.summary.Summary.Image(encoded_image_string=s.getvalue(), height=image_numpy.shape[0], width=image_numpy.shape[1])
                 # Create a Summary value
-                img_summaries.append(self.tf.Summary.Value(tag=label, image=img_sum))
+                img_summaries.append(self.tf.compat.v1.summary.Summary.Value(tag=label, image=img_sum))
 
             # Create and write Summary
-            summary = self.tf.Summary(value=img_summaries)
+            summary = self.tf.compat.v1.summary.Summary(value=img_summaries)
             self.writer.add_summary(summary, step)
 
         if self.use_html: # save images to a html file
@@ -95,7 +97,7 @@ class Visualizer():
     def plot_current_errors(self, errors, step):
         if self.tf_log:
             for tag, value in errors.items():
-                summary = self.tf.Summary(value=[self.tf.Summary.Value(tag=tag, simple_value=value)])
+                summary = self.tf.compat.v1.summary.Summary(value=[self.tf.compat.v1.summary.Summary.Value(tag=tag, simple_value=value)])
                 self.writer.add_summary(summary, step)
 
     # errors: same format as |errors| of plotCurrentErrors
